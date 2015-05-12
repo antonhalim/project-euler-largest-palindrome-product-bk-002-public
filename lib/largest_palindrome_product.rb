4fdc15d4699f8fb3d5f06a97ee2248cd414ab69a
axton21 <axton_n@yahoo.com>
2015-05-12 09:30:15 -0400

    done

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,23 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+	answer = 0
+	a, b, c, d = 100, 100, 999, 999
+	for i in a..c do
+		for k in a..c do
+			if palindrome(i*k)
+				if answer < i*k
+					answer = i*k
+				end
+			end
+		end
+	end
+	return answer
+end
+
+private
+def palindrome(number)
+	if number.to_s.split("") == number.to_s.split("").reverse
+		return true
+	else
+		return false
+	end
+end
